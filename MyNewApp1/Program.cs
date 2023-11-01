981ff2e832fd8513592f5d2135747aabe511d2d4
zumnausman <zumna.u@gmail.com>
2023-11-01 19:25:14 +0500

    aaa

--- MyNewApp1/Program.cs
+++ MyNewApp1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World 222!");
+Console.WriteLine("Hello, World 222333!");
+
+
